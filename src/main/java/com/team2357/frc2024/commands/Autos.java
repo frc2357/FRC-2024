fe5b3e365e8dcb1b72bc2e76685ca4ad122dbef1
MidgleyM <115593302+MidgleyM@users.noreply.github.com>
2024-01-08 13:32:14 -0600

    fixed a small issue with an exaple file, so it builds

--- src/main/java/com/team2357/frc2024/commands/Autos.java
+++ src/main/java/com/team2357/frc2024/commands/Autos.java
@@ -6,6 +6,7 @@ package com.team2357.frc2024.commands;
 
 import com.team2357.frc2024.subsystems.ExampleSubsystem;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 
