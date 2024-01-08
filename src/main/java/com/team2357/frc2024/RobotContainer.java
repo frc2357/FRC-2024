ec1540cf47a83c82b8a2251d0894af4ea9e2c40f
MidgleyM <115593302+MidgleyM@users.noreply.github.com>
2024-01-08 12:29:48 -0600

    fixed package declerations

--- src/main/java/com/team2357/frc2024/RobotContainer.java
+++ src/main/java/com/team2357/frc2024/RobotContainer.java
@@ -2,12 +2,13 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
+package com.team2357.frc2024;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import com.team2357.frc2024.Constants.OperatorConstants;
+import com.team2357.frc2024.commands.Autos;
+import com.team2357.frc2024.commands.ExampleCommand;
+import com.team2357.frc2024.subsystems.ExampleSubsystem;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
--- src/main/java/com/team2357/frc2024/commands/Autos.java
+++ src/main/java/com/team2357/frc2024/commands/Autos.java
@@ -2,9 +2,10 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package com.team2357.frc2024.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
+import com.team2357.frc2024.subsystems.ExampleSubsystem;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 
