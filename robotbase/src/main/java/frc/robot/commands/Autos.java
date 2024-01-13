84c165206489178658614253a3f7a82bb25db628
MidgleyM <115593302+MidgleyM@users.noreply.github.com>
2024-01-13 12:35:43 -0600

    fixed some issues that happened with files that didnt want to run now that they were in main

--- robotbase/src/main/java/frc/robot/commands/Autos.java
+++ robotbase/src/main/java/frc/robot/commands/Autos.java
@@ -5,13 +5,19 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import java.util.Set;
 
 public final class Autos {
   /** Example static factory for an autonomous command. */
   public static Command exampleAuto() {
-    return new Command(){
-      
+    return new Command() {
+
+      @Override
+      public Set<Subsystem> getRequirements() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRequirements'");
+      }
     };
   }
 
