a554d58740b014c4a9d63e345f895aaacd0d296e
Tyson453 <95186148+Tyson453@users.noreply.github.com>
2024-01-19 17:23:11 -0800

    Create shooter axis and stop commands

---
+++ robotbase/src/main/java/frc/robot/commands/shooter/ShooterRollerStepAxisCommand.java
@@ -0,0 +1,38 @@
+package frc.robot.commands.shooter;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Robot;
+import frc.robot.controls.util.AxisInterface;
+
+public class ShooterRollerStepAxisCommand extends Command {
+    private AxisInterface m_axis;
+    private double m_step;
+
+    /**
+     * Same as ShooterRollerAxisCommand but rounds to the nearest step
+     * @param axis Controller axis
+     * @param step value from 0 to 1
+     */
+    public ShooterRollerStepAxisCommand(AxisInterface axis, double step) {
+        m_axis = axis;
+        m_step = step;
+        addRequirements(Robot.shooter);
+    }
+
+    @Override
+    public void execute() {
+        double axisValue = m_axis.getValue();
+        axisValue = Math.floor(axisValue / m_step) * m_step;
+        Robot.shooter.setAxisSpeed(axisValue, axisValue);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        Robot.shooter.stop();
+    }
+}
