3953174a8f2e5e11ad8ff502c701303c96678945
Tyson453 <95186148+Tyson453@users.noreply.github.com>
2024-01-19 17:38:45 -0800

    Add controls

--- robotbase/src/main/java/frc/robot/commands/shooter/ShooterRollerAxisCommand.java
+++ robotbase/src/main/java/frc/robot/commands/shooter/ShooterRollerAxisCommand.java
@@ -5,26 +5,26 @@ import frc.robot.Robot;
 import frc.robot.controls.util.AxisInterface;
 
 public class ShooterRollerAxisCommand extends Command {
-    private AxisInterface m_axis;
+  private AxisInterface m_axis;
 
-    public ShooterRollerAxisCommand(AxisInterface axis) {
-        m_axis = axis;
-        addRequirements(Robot.shooter);
-    }
+  public ShooterRollerAxisCommand(AxisInterface axis) {
+    m_axis = axis;
+    addRequirements(Robot.shooter);
+  }
 
-    @Override
-    public void execute() {
-        double axisValue = m_axis.getValue();
-        Robot.shooter.setAxisSpeed(axisValue, axisValue);
-    }
+  @Override
+  public void execute() {
+    double axisValue = m_axis.getValue();
+    Robot.shooter.setAxisSpeed(axisValue, axisValue);
+  }
 
-    @Override
-    public boolean isFinished() {
-        return false;
-    }
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
 
-    @Override
-    public void end(boolean interrupted) {
-        Robot.shooter.stop();
-    }
+  @Override
+  public void end(boolean interrupted) {
+    Robot.shooter.stop();
+  }
 }
--- robotbase/src/main/java/frc/robot/commands/shooter/ShooterRollerStopCommand.java
+++ robotbase/src/main/java/frc/robot/commands/shooter/ShooterRollerStopCommand.java
@@ -4,17 +4,17 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Robot;
 
 public class ShooterRollerStopCommand extends Command {
-    public ShooterRollerStopCommand() {
-        addRequirements(Robot.shooter);
-    }
+  public ShooterRollerStopCommand() {
+    addRequirements(Robot.shooter);
+  }
 
-    @Override
-    public boolean isFinished() {
-        return true;
-    }
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
 
-    @Override
-    public void end(boolean interrupted) {
-        Robot.shooter.stop();
-    }
+  @Override
+  public void end(boolean interrupted) {
+    Robot.shooter.stop();
+  }
 }
