1cafa5f86838b30dadda38c820a126af37703be4
Tyson453 <95186148+Tyson453@users.noreply.github.com>
2024-01-20 10:06:35 -0800

    Linting

--- robotbase/src/main/java/frc/robot/commands/pivot/ShooterPivotAxisCommand.java
+++ robotbase/src/main/java/frc/robot/commands/pivot/ShooterPivotAxisCommand.java
@@ -5,27 +5,26 @@ import frc.robot.Robot;
 import frc.robot.controls.util.AxisInterface;
 
 public class ShooterPivotAxisCommand extends Command {
-    private AxisInterface m_axis;
+  private AxisInterface m_axis;
 
-    public ShooterPivotAxisCommand(AxisInterface axis) {
-        m_axis = axis;
-        addRequirements(Robot.pivot);
-    }
+  public ShooterPivotAxisCommand(AxisInterface axis) {
+    m_axis = axis;
+    addRequirements(Robot.pivot);
+  }
 
-    @Override
-    public void execute() {
-        double axisSpeed = m_axis.getValue();
-        Robot.pivot.setPivotAxisSpeed(axisSpeed);
-    }
+  @Override
+  public void execute() {
+    double axisSpeed = m_axis.getValue();
+    Robot.pivot.setPivotAxisSpeed(axisSpeed);
+  }
 
-    @Override
-    public boolean isFinished() {
-        return false;
-    }
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
 
-    @Override
-    public void end(boolean interrupted) {
-        Robot.pivot.stop();
-    }
-    
+  @Override
+  public void end(boolean interrupted) {
+    Robot.pivot.stop();
+  }
 }
--- robotbase/src/main/java/frc/robot/commands/pivot/ShooterPivotStopCommand.java
+++ robotbase/src/main/java/frc/robot/commands/pivot/ShooterPivotStopCommand.java
@@ -4,17 +4,17 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Robot;
 
 public class ShooterPivotStopCommand extends Command {
-    public ShooterPivotStopCommand() {
-        addRequirements(Robot.pivot);
-    }
+  public ShooterPivotStopCommand() {
+    addRequirements(Robot.pivot);
+  }
 
-    @Override
-    public boolean isFinished() {
-        return true;
-    }
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
 
-    @Override
-    public void end(boolean interrupted) {
-        Robot.pivot.stop();
-    }
+  @Override
+  public void end(boolean interrupted) {
+    Robot.pivot.stop();
+  }
 }
