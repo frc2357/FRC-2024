3826f2a05b2cb4f45c795abd4e920fc2d86127b9
Tyson453 <95186148+Tyson453@users.noreply.github.com>
2024-01-20 08:35:41 -0800

    Create tuning project (#18)
    * made the ChoreoTrajectoryCommand work
    
    * it works, but nolan says something wrong somewhere
    
    * Create ShooterSubsystem
    
    * Start fixing limelight
    
    * made Choreo work, and added methods to the swerve sub
    
    * linting
    
    * Refactor limelight subsystem
    
    * Different pipelines for shooting
    
    * Create robot state
    
    * Display Alliance in smart dashboard
    
    * Linting
    
    * Refactor controls
    
    * Linting
    
    * Create tuning project
    
    * Linting
    
    * Linting
    
    * made review change
    
    * No more singleton
    
    * Refactor controls
    
    * Linting
    
    * Add alliance selection on codrive controls
    
    * Create ShooterSubsystem
    
    * Start fixing limelight
    
    * Refactor limelight subsystem
    
    * Different pipelines for shooting
    
    * Linting
    
    * linting
    
    * Finish up the logic for shooting
    
    * linting yet again
    
    * made the autos use more accurate measurments, should be more realistic
    
    * made a rotation accuracy test, and made the 3 ring maybe more reliable
    
    * Create shooter pivot subsystem
    
    * create intake subsystem
    
    * Fix changes from main
    
    * Linting
    
    * Only one P
    
    * Subsystems, controls, pids, SmartDashboard, and more
    
    * Create tuning project
    
    * Subsystems, controls, pids, SmartDashboard, and more
    
    * Linting
    
    * Fix controls and no command base
    
    ---------
    
    Co-authored-by: MidgleyM <115593302+MidgleyM@users.noreply.github.com>

---
+++ tuning/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,117 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.subsystems.IntakeTuningSubsystem;
+import frc.robot.subsystems.ShooterPivotTuningSubsystem;
+import frc.robot.subsystems.ShooterTuningSubsystem;
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  private XboxController m_controller;
+
+  private static IntakeTuningSubsystem intake;
+  private static ShooterTuningSubsystem shooter;
+  private static ShooterPivotTuningSubsystem pivot;
+
+  private JoystickButton m_rightBumper;
+  private JoystickButton m_leftBumper;
+
+  private boolean m_intakeInverted = false;
+  private boolean m_shooterInverted = false;
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    m_controller = new XboxController(0);
+
+    intake = new IntakeTuningSubsystem();
+    shooter = new ShooterTuningSubsystem();
+    pivot = new ShooterPivotTuningSubsystem();
+
+    m_rightBumper = new JoystickButton(m_controller, Button.kRightBumper.value);
+    m_leftBumper = new JoystickButton(m_controller, Button.kLeftBumper.value);
+  }
+
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+  }
+
+  /** This function is called once each time the robot enters Disabled mode. */
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  @Override
+  public void autonomousInit() {
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {
+  }
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {
+    intake.update();
+    shooter.update();
+    pivot.update();
+  }
+
+  @Override
+  public void testInit() {
+  }
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {
+    double leftTrigger = m_controller.getLeftTriggerAxis();
+    intake.axisRun(leftTrigger, leftTrigger, m_intakeInverted);
+
+    double rightTrigger = m_controller.getRightTriggerAxis();
+    shooter.axisRun(rightTrigger, rightTrigger, m_shooterInverted);
+
+    double rightJoystick = m_controller.getRightY();
+    pivot.axisRun(rightJoystick);
+
+    m_rightBumper.onTrue(new InstantCommand(() -> m_shooterInverted = !m_shooterInverted));
+    m_leftBumper.onTrue(new InstantCommand(() -> m_intakeInverted = !m_intakeInverted));
+  }
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
+}
