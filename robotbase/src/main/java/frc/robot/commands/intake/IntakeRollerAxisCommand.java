3ddc56fa9629f94c36b952ddb403967ace2292f3
frc2357 <personman6000@gmail.com>
2024-01-25 17:24:35 -0600

    Added Intake Commands

---
+++ robotbase/src/main/java/frc/robot/commands/intake/IntakeRollerAxisCommand.java
@@ -0,0 +1,35 @@
+package frc.robot.commands.intake;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Robot;
+import frc.robot.controls.util.AxisInterface;
+
+public class IntakeRollerAxisCommand extends Command {
+    private AxisInterface m_axis;
+
+    public IntakeRollerAxisCommand(AxisInterface axis) {
+        m_axis = axis;
+        addRequirements(Robot.intake);
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void execute() {
+        double axisValue = m_axis.getValue();
+        Robot.intake.setAxisSpeed(axisValue,axisValue);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        Robot.intake.stop();
+    }
+}
