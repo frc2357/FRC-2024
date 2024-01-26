e31f04bbac01b22dc515f64e74b7d11c23e20932
frc2357 <personman6000@gmail.com>
2024-01-25 18:36:23 -0600

    Added the Intake Stop command, for real this time.

---
+++ robotbase/src/main/java/frc/robot/commands/intake/IntakeRollerStopCommand.java
@@ -0,0 +1,23 @@
+package frc.robot.commands.intake;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Robot;
+
+public class IntakeRollerStopCommand extends Command {
+    
+    public IntakeRollerStopCommand() {
+        addRequirements(Robot.intake);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        Robot.intake.stop();
+    }
+
+
+}
