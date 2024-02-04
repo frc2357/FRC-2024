496ab345a05b72a58df8d11b872e3b85ac58655b
Tyson453 <95186148+Tyson453@users.noreply.github.com>
2024-02-03 18:07:56 -0800

    Whoops

--- robotbase/src/main/java/frc/robot/commands/DriveToGamepeiceCommand.java
+++ robotbase/src/main/java/frc/robot/commands/DriveToGamepeiceCommand.java
@@ -1,22 +1,21 @@
 package frc.robot.commands;
 
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.Constants;
-import frc.robot.Robot;
 import frc.robot.Constants.SWERVE;
+import frc.robot.Robot;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
-import frc.robot.subsystems.LimelightSubsystem;
+import frc.robot.subsystems.Limelight;
 import frc.robot.util.Utility;
 
-public class DriveToGamepeiceCommand extends Command{
-    private static LimelightSubsystem limelightSub = Robot.shooterLimelight;
+public class DriveToGamepeiceCommand extends Command {
+    private static Limelight limelightSub = Robot.shooterLimelight;
     private static CommandSwerveDrivetrain driveSub = Robot.drive;
     private int hangTimer = 0;
 
     public DriveToGamepeiceCommand() {
-        addRequirements(new Subsystem[]{Robot.drive, Robot.shooterLimelight});
+        addRequirements(new Subsystem[] { Robot.drive, Robot.shooterLimelight });
     }
 
     @Override
@@ -35,17 +34,18 @@ public class DriveToGamepeiceCommand extends Command{
 
     @Override
     public boolean isFinished() {
-        if(!limelightSub.validTargetExists()){
+        if (!limelightSub.validTargetExists()) {
             hangTimer++;
-        }
-        else{
+        } else {
             hangTimer = 0;
         }
 
-        return 
-            (Utility.isWithinTolerance(limelightSub.getTX(), Constants.SWERVE.GAMEPIECE_TRACKING_ROTATION_OFFSET, Constants.SWERVE.GAMEPIECE_TRACKING_ROTATION_TOLERANCE) &&
-            Utility.isWithinTolerance(limelightSub.getTY(), Constants.SWERVE.GAMEPIECE_TRACKING_TRANSLATION_OFFSET, Constants.SWERVE.GAMEPIECE_TRACKING_TRANSLATION_TOLERANCE))
-            & (hangTimer >= Constants.SWERVE.GAMEPIECE_TRACKING_LOST_TARGET_ALLOWED_LOOPS || limelightSub.validTargetExists());
+        return (Utility.isWithinTolerance(limelightSub.getTX(), Constants.SWERVE.GAMEPIECE_TRACKING_ROTATION_OFFSET,
+                Constants.SWERVE.GAMEPIECE_TRACKING_ROTATION_TOLERANCE) &&
+                Utility.isWithinTolerance(limelightSub.getTY(), Constants.SWERVE.GAMEPIECE_TRACKING_TRANSLATION_OFFSET,
+                        Constants.SWERVE.GAMEPIECE_TRACKING_TRANSLATION_TOLERANCE))
+                & (hangTimer >= Constants.SWERVE.GAMEPIECE_TRACKING_LOST_TARGET_ALLOWED_LOOPS
+                        || limelightSub.validTargetExists());
     }
 
     @Override
