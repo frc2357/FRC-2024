c3b120303c2903ceef23c5d1df6ad9074bd8b7e3
frc2357 <personman6000@gmail.com>
2024-02-08 17:45:25 -0600

    ran the gradle

--- robotbase/src/main/java/frc/robot/commands/intake/IntakeToShooterCommand.java
+++ robotbase/src/main/java/frc/robot/commands/intake/IntakeToShooterCommand.java
@@ -4,30 +4,29 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.Robot;
 
-public class IntakeToShooterCommand extends Command{
+public class IntakeToShooterCommand extends Command {
 
-    public IntakeToShooterCommand(){
-        addRequirements(Robot.intake);
-    }
+  public IntakeToShooterCommand() {
+    addRequirements(Robot.intake);
+  }
 
-    @Override 
-    public void initialize() {}
+  @Override
+  public void initialize() {}
 
-    @Override
-    public void execute(){
-        Robot.intake.setAxisSpeed(
-            Constants.INTAKE.TOP_MOTOR_SPEED_PERCENT_OUTPUT,
-            Constants.INTAKE.BOTTOM_MOTOR_SPEED_PERCENT_OUTPUT);
-    }
+  @Override
+  public void execute() {
+    Robot.intake.setAxisSpeed(
+        Constants.INTAKE.TOP_MOTOR_SPEED_PERCENT_OUTPUT,
+        Constants.INTAKE.BOTTOM_MOTOR_SPEED_PERCENT_OUTPUT);
+  }
 
-    @Override
-    public boolean isFinished() {
-        return false;
-    }
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
 
-    @Override
-    public void end(boolean interrupted) {
-        Robot.intake.stop();
-    }
+  @Override
+  public void end(boolean interrupted) {
+    Robot.intake.stop();
+  }
 }
-
