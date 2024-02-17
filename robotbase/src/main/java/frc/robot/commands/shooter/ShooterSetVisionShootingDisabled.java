df7bacd389281bd9354c64bc25cac78ed86c2957
Max Midgley <115593302+MidgleyM@users.noreply.github.com>
2024-02-17 13:48:24 -0600

    shooter disable vision shooting command (#105)

---
+++ robotbase/src/main/java/frc/robot/commands/shooter/ShooterSetVisionShootingDisabled.java
@@ -0,0 +1,18 @@
+package frc.robot.commands.shooter;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Robot;
+
+public class ShooterSetVisionShootingDisabled extends Command {
+  public ShooterSetVisionShootingDisabled() {}
+
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    Robot.shooter.stopVisionShooting();
+  }
+}
