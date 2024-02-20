6dcef18e0b8a38e1dbf04193cf4de61c3d0522bf
MidgleyM <115593302+MidgleyM@users.noreply.github.com>
2024-02-20 17:18:22 -0600

    part of review changes

--- robotbase/src/main/java/frc/robot/commands/drive/ShooterTargetLock.java
+++ robotbase/src/main/java/frc/robot/commands/drive/ShooterTargetLock.java
@@ -4,25 +4,29 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.SWERVE;
 import frc.robot.Robot;
 import frc.robot.state.RobotState.DriveControlState;
+import frc.robot.subsystems.PhotonVisionCamera;
 
 public class ShooterTargetLock extends Command {
   private int m_pipelineIndex;
+  private PhotonVisionCamera m_camera;
+  private int m_deafaultPipeline;
 
-  public ShooterTargetLock(int pipelineIndex) {
+  public ShooterTargetLock(int pipelineIndex, PhotonVisionCamera camera) {
     m_pipelineIndex = pipelineIndex;
+    m_camera = camera;
   }
 
   @Override
   public void initialize() {
     Robot.state.setDriveControlState(DriveControlState.TARGET_LOCK);
-    Robot.shooterCam.setPipeline(m_pipelineIndex);
+    m_camera.setPipeline(m_pipelineIndex);
     SWERVE.ROTATION_PID_CONTROLLER.reset();
   }
 
   @Override
   public void end(boolean interrupted) {
     Robot.state.setDriveControlState(DriveControlState.FIELD_RELATIVE);
-    Robot.shooterCam.setDefaultPipelineActive();
+    m_camera.setPipeline(m_deafaultPipeline);
     SWERVE.ROTATION_PID_CONTROLLER.setP(SWERVE.ROTATION_KP);
   }
 }
