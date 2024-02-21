63769146c5d7b51cecc2ffad143f5d52b11a5b12
frc2357 <personman6000@gmail.com>
2024-02-20 18:32:59 -0600

    pushing before I leave, not finished

--- robotbase/src/main/java/frc/robot/commands/scoring/SpeakerScoreFromBase.java
+++ robotbase/src/main/java/frc/robot/commands/scoring/SpeakerScoreFromBase.java
@@ -1,5 +1,7 @@
 package frc.robot.commands.scoring;
 
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.*;
 import frc.robot.commands.intake.IntakeFeedToShooter;
@@ -12,14 +14,15 @@ import frc.robot.commands.shooter.ShooterWaitForRPMs;
 public class SpeakerScoreFromBase extends SequentialCommandGroup {
   public SpeakerScoreFromBase() {
     super(
-        new ShooterSetRPMs(
-            SHOOTER.TOP_MOTOR_SPEAKER_SCORE_FROM_BASE_RPMS,
-            SHOOTER.BOTTOM_MOTOR_SPEAKER_SCORE_FROM_BASE_RPMS),
-        new PivotSetRotation(PIVOT.SPEAKER_SCORE_FROM_BASE_ROTATION),
+        new ParallelDeadlineGroup(
+            new PivotSetRotation(PIVOT.SPEAKER_SCORE_FROM_BASE_ROTATION),
+            new ShooterSetRPMs(
+                SHOOTER.TOP_MOTOR_SPEAKER_SCORE_FROM_BASE_RPMS,
+                SHOOTER.BOTTOM_MOTOR_SPEAKER_SCORE_FROM_BASE_RPMS)),
         new ShooterWaitForRPMs(),
         new IntakeFeedToShooter().withTimeout(INTAKE.FEED_TO_SHOOTER_TIMEOUT),
-        new ShooterStop(),
-        new IntakeStop()
+        new IntakeStop(),
+        new ShooterStop()
         // new SetRobotStateCommand(RobotState.State.EMPTY)
 
         );
