89d6c02af22f1b10d75901267fbde99251d27d49
frc2357 <personman6000@gmail.com>
2024-02-22 17:26:12 -0600

    changed the parallel deadline group to a regular parallel command group

--- robotbase/src/main/java/frc/robot/commands/scoring/SpeakerScoreFromBase.java
+++ robotbase/src/main/java/frc/robot/commands/scoring/SpeakerScoreFromBase.java
@@ -1,6 +1,6 @@
 package frc.robot.commands.scoring;
 
-import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.*;
 import frc.robot.commands.intake.IntakeFeedToShooter;
@@ -12,11 +12,12 @@ import frc.robot.commands.shooter.ShooterWaitForRPMs;
 public class SpeakerScoreFromBase extends SequentialCommandGroup {
   public SpeakerScoreFromBase() {
     super(
-        new ParallelDeadlineGroup(
+        new ParallelCommandGroup(
             new PivotSetRotation(PIVOT.SUBWOOFER_SHOT_ROTATION),
             new ShooterSetRPMs(
-                SHOOTER.TOP_MOTOR_SUBWOOFER_SHOT_RPMS, SHOOTER.BOTTOM_MOTOR_SUBWOOFER_SHOT_RPMS)),
-        new ShooterWaitForRPMs(),
+                SHOOTER.TOP_MOTOR_SUBWOOFER_SHOT_RPMS, SHOOTER.BOTTOM_MOTOR_SUBWOOFER_SHOT_RPMS),
+                new ShooterWaitForRPMs()),
+        
         new IntakeFeedToShooter().withTimeout(INTAKE.FEED_TO_SHOOTER_TIMEOUT),
         new ShooterStop()
         // new SetRobotStateCommand(RobotState.State.EMPTY)
