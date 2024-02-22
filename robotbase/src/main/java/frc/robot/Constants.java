6e6b91dd86a074a1c573dd035a9e3b7ad69e8929
MidgleyM <115593302+MidgleyM@users.noreply.github.com>
2024-02-21 18:19:13 -0600

    shooter camera transform done

--- robotbase/src/main/java/frc/robot/Constants.java
+++ robotbase/src/main/java/frc/robot/Constants.java
@@ -11,6 +11,7 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
 import java.util.function.BooleanSupplier;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 
@@ -357,7 +358,16 @@ public final class Constants {
 
     public static final double HEAD_ON_TOLERANCE = 0;
 
+    public static final double LENS_BEHIND_OF_ROBOT_ORIGIN_INCHES = 8.172;
+    public static final double LENS_TO_RIGHT_OF_ROBOT_ORIGIN_INCHES = 8.45;
+    public static final double LENS_HEIGHT_FROM_ROBOT_IRIGIN = 13.388;
+    public static final double LENS_ANGLE_TILTED_UP_DEGREES = 30;
     public static final Transform3d ROBOT_TO_CAMERA_TRANSFORM =
-        new Transform3d(new Translation3d(0, 0, 0), new Rotation3d(0, 0, 0));
+        new Transform3d(
+            new Translation3d(
+                -Units.inchesToMeters(LENS_BEHIND_OF_ROBOT_ORIGIN_INCHES),
+                -Units.inchesToMeters(LENS_TO_RIGHT_OF_ROBOT_ORIGIN_INCHES),
+                Units.inchesToMeters(LENS_HEIGHT_FROM_ROBOT_IRIGIN)),
+            new Rotation3d(0, LENS_ANGLE_TILTED_UP_DEGREES, 0));
   }
 }
