cd964af8a604d44b1eb136e7c794c019b85166a7
Tyson453 <95186148+Tyson453@users.noreply.github.com>
2024-02-24 15:36:37 -0800

    Linting

--- robotbase/src/main/java/frc/robot/Constants.java
+++ robotbase/src/main/java/frc/robot/Constants.java
@@ -16,15 +16,11 @@ import java.util.function.BooleanSupplier;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide
- * numerical or boolean
- * constants. This class should not be used for any other purpose. All constants
- * should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>
- * It is advised to statically import this class (or one of its inner classes)
- * wherever the
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
@@ -92,23 +88,28 @@ public final class Constants {
     public static final double PIECE_TRACKING_ROTATION_TOLERANCE = 0.1;
 
     public static final double PIECE_TRACKING_MAX_DISTANCE_METERS = 3.0; // In Meters
-    public static final double PIECE_TRACKING_SLOW_DOWN_METERS = 1.0; // Robot goes half speed once passed
+    public static final double PIECE_TRACKING_SLOW_DOWN_METERS =
+        1.0; // Robot goes half speed once passed
     public static final double PIECE_TRACKING_X_METERS_PER_SECOND = 2;
 
     // Target Lock
     public static final double TARGET_LOCK_ROTATION_KP = 0.15;
     public static final double TARGET_LOCK_ROTATION_KI = 0.0;
     public static final double TARGET_LOCK_ROTATION_KD = 0.0;
-    public static final PIDController TARGET_LOCK_ROTATION_PID_CONTROLLER = new PIDController(
-        TARGET_LOCK_ROTATION_KP, TARGET_LOCK_ROTATION_KI, TARGET_LOCK_ROTATION_KD);
+    public static final PIDController TARGET_LOCK_ROTATION_PID_CONTROLLER =
+        new PIDController(
+            TARGET_LOCK_ROTATION_KP, TARGET_LOCK_ROTATION_KI, TARGET_LOCK_ROTATION_KD);
 
     public static final double TARGET_LOCK_FEED_FORWARD = 0.0;
     public static final double TARGET_LOCK_TOLERANCE = 0.25;
 
     // Translate to Apriltag
-    public static final PIDController APRILTAG_ROTATION_PID_CONTROLLER = new PIDController(0.03, 0, 0.01);
-    public static final PIDController APRILTAG_X_TRANSLATION_PID_CONTROLLER = new PIDController(0.05, 0, 0);
-    public static final PIDController APRILTAG_Y_TRANSLATION_PID_CONTROLLER = new PIDController(0.08, 0, 0);
+    public static final PIDController APRILTAG_ROTATION_PID_CONTROLLER =
+        new PIDController(0.03, 0, 0.01);
+    public static final PIDController APRILTAG_X_TRANSLATION_PID_CONTROLLER =
+        new PIDController(0.05, 0, 0);
+    public static final PIDController APRILTAG_Y_TRANSLATION_PID_CONTROLLER =
+        new PIDController(0.08, 0, 0);
 
     public static final double APRILTAG_X_TOLERANCE = 0.5;
     public static final double APRILTAG_Y_TOLERANCE = 0.5;
@@ -125,12 +126,13 @@ public final class Constants {
     public static final PIDController Y_CONTROLLER = new PIDController(2, 0, 0);
     public static final PIDController ROTATION_CONTROLLER = new PIDController(0.6, 0, 0);
 
-    public static final BooleanSupplier CHOREO_AUTO_MIRROR_PATHS = new BooleanSupplier() {
-      @Override
-      public boolean getAsBoolean() {
-        return false;
-      }
-    };
+    public static final BooleanSupplier CHOREO_AUTO_MIRROR_PATHS =
+        new BooleanSupplier() {
+          @Override
+          public boolean getAsBoolean() {
+            return false;
+          }
+        };
   }
 
   public static final class CONTROLLER {
@@ -346,8 +348,8 @@ public final class Constants {
   }
 
   public static final class PHOTON_VISION {
-    public static final AprilTagFieldLayout APRIL_TAG_FIELD_LAYOUT = AprilTagFields.k2024Crescendo
-        .loadAprilTagLayoutField();
+    public static final AprilTagFieldLayout APRIL_TAG_FIELD_LAYOUT =
+        AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
     public static final PoseStrategy POSE_STRATEGY = PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR;
   }
 
@@ -366,11 +368,12 @@ public final class Constants {
     public static final double LENS_TO_RIGHT_OF_ROBOT_ORIGIN_INCHES = 8.45;
     public static final double LENS_HEIGHT_FROM_ROBOT_ORIGIN_INCHES = 13.388;
     public static final double LENS_ANGLE_TILTED_UP_DEGREES = 30;
-    public static final Transform3d ROBOT_TO_CAMERA_TRANSFORM = new Transform3d(
-        new Translation3d(
-            -Units.inchesToMeters(LENS_BEHIND_OF_ROBOT_ORIGIN_INCHES),
-            -Units.inchesToMeters(LENS_TO_RIGHT_OF_ROBOT_ORIGIN_INCHES),
-            Units.inchesToMeters(LENS_HEIGHT_FROM_ROBOT_ORIGIN_INCHES)),
-        new Rotation3d(0, LENS_ANGLE_TILTED_UP_DEGREES, 0));
+    public static final Transform3d ROBOT_TO_CAMERA_TRANSFORM =
+        new Transform3d(
+            new Translation3d(
+                -Units.inchesToMeters(LENS_BEHIND_OF_ROBOT_ORIGIN_INCHES),
+                -Units.inchesToMeters(LENS_TO_RIGHT_OF_ROBOT_ORIGIN_INCHES),
+                Units.inchesToMeters(LENS_HEIGHT_FROM_ROBOT_ORIGIN_INCHES)),
+            new Rotation3d(0, LENS_ANGLE_TILTED_UP_DEGREES, 0));
   }
 }
