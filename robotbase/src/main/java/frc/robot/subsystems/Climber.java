5dd939ad3ca467f00c0966a84299525c895f3050
coderkevin <kevin@redfenix.com>
2024-02-27 21:53:23 -0600

    Formatting

--- robotbase/src/main/java/frc/robot/subsystems/Climber.java
+++ robotbase/src/main/java/frc/robot/subsystems/Climber.java
@@ -3,7 +3,6 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
--- robotbase/src/main/java/frc/robot/subsystems/Pivot.java
+++ robotbase/src/main/java/frc/robot/subsystems/Pivot.java
@@ -6,7 +6,6 @@ import com.revrobotics.CANSparkLowLevel.PeriodicFrame;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkAbsoluteEncoder;
 import com.revrobotics.SparkPIDController;
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
