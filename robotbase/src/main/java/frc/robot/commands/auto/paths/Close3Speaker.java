d7089a3f10fdf423825a42c3b0780efe518f46c0
Tyson453 <95186148+Tyson453@users.noreply.github.com>
2024-02-29 22:23:47 -0800

    2 GAME PIECES

--- robotbase/src/main/java/frc/robot/commands/auto/paths/Close3Speaker.java
+++ robotbase/src/main/java/frc/robot/commands/auto/paths/Close3Speaker.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.drive.DriveChoreoPath;
+import frc.robot.commands.intake.IntakeRun;
 import frc.robot.commands.pivot.PivotHoldAngle;
 import frc.robot.commands.shooter.ShooterSetRPM;
 
@@ -16,9 +17,12 @@ public class Close3Speaker extends SequentialCommandGroup {
                 new WaitCommand(1),
                 new DriveChoreoPath("Close3Speaker.2"),
                 new WaitCommand(1),
-                new DriveChoreoPath("Close3Speaker.3")),
-            new PivotHoldAngle(90, true),
-            new ShooterSetRPM(1000, true)));
+                new DriveChoreoPath("Close3Speaker.3"),
+                new WaitCommand(2)),
+            
+            new PivotHoldAngle(55, true),
+            new IntakeRun(0.75),
+            new ShooterSetRPM(4000, true)));
   }
 
   @Override
---
+++ robotbase/src/main/java/frc/robot/commands/intake/IntakeRun.java
@@ -0,0 +1,36 @@
+package frc.robot.commands.intake;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Robot;
+
+public class IntakeRun extends Command {
+   private double m_speed;
+   private boolean m_stopOnEnd;
+
+   public IntakeRun(double speed) {
+      this(speed, true);
+   }
+
+   public IntakeRun(double speed, boolean stopOnEnd) {
+      m_speed = speed;
+      m_stopOnEnd = stopOnEnd;
+      addRequirements(Robot.intake);
+   }
+
+   @Override
+   public void initialize() {
+      Robot.intake.set(m_speed);
+   }
+
+   @Override
+   public boolean isFinished() {
+      return false;
+   }
+
+   @Override
+   public void end(boolean interrupted) {
+      if (m_stopOnEnd) {
+         Robot.intake.stop();
+      }
+   }
+}
