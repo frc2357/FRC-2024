dadec0b1398a2e4c81ae137bea4f66608fa13bc2
NolanCampbell2357 <nolanc1223@gmail.com>
2024-03-02 11:13:32 -0600

    Center note command

--- robotbase/src/main/java/frc/robot/commands/intake/IntakeNoteFromFloor.java
+++ robotbase/src/main/java/frc/robot/commands/intake/IntakeNoteFromFloor.java
@@ -2,6 +2,7 @@ package frc.robot.commands.intake;
 
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants.INTAKE;
 import frc.robot.commands.rumble.RumbleDriverController;
 import frc.robot.commands.state.SetNoteState;
@@ -10,16 +11,18 @@ import frc.robot.state.RobotState.NoteState;
 public class IntakeNoteFromFloor extends SequentialCommandGroup {
   public IntakeNoteFromFloor() {
     super(
+        // Run until sensor sees the intake
         new IntakeRunUntilBeamState(INTAKE.PICKUP_SPEED_PERCENT_OUTPUT, true),
         new SetNoteState(NoteState.NOTE_IN_INTAKE),
+
+        // Run note up against shooter rollers to compress it
         new ParallelDeadlineGroup(
-            new IntakeRunUntilBeamState(INTAKE.SLOW_PICKUP_SPEED_PERCENT_OUTPUT, false),
-            new RumbleDriverController()),
-        new SetNoteState(NoteState.NOTE_PAST_BEAM_BREAK),
-        // new ParallelDeadlineGroup(
-        // new WaitCommand(INTAKE.FLOOR_INTAKE_REVERSE_TIMEOUT),
-        // new InstantCommand(() ->
-        // Robot.intake.set(INTAKE.REVERSE_FEED_SPEED_PERCENT_OUTPUT))),
+            new WaitCommand(INTAKE.COMPRESS_NOTE_TIMEOUT),
+            new RumbleDriverController(),
+            new IntakeRun(INTAKE.COMPRESS_NOTE_SPEED_PERCENT_OUTPUT, true)),
+            
+        // Run the note back down until the sensor sees it
+        new IntakeRunUntilBeamState(INTAKE.REVERSE_FEED_SPEED_PERCENT_OUTPUT, true),
         new IntakeStop(),
         new SetNoteState(NoteState.NOTE_STOWED));
   }
