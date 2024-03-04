440805c27ad7190a90c60b1bc87c677740022d31
frc2357 <personman6000@gmail.com>
2024-03-04 16:45:25 -0600

    gradle

--- robotbase/src/main/java/frc/robot/Constants.java
+++ robotbase/src/main/java/frc/robot/Constants.java
@@ -335,11 +335,10 @@ public final class Constants {
     public static final int PORT_NUMBER = 0;
     public static final int STRIP_LENGTH = 30;
 
-
-    public static final Color WHITE = new Color(255,255,255);
+    public static final Color WHITE = new Color(255, 255, 255);
     public static final Color GREEN = new Color(0, 255, 0);
     public static final Color RED = new Color(255, 0, 0);
-    public static final Color BLUE = new Color(0,0,255);
+    public static final Color BLUE = new Color(0, 0, 255);
     public static final Color YELLOW = new Color(255, 200, 5);
     public static final Color ORANGE = new Color(255, 35, 0);
   }
