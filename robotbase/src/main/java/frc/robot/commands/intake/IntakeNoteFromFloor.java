a48f8dae22eb4f2c6c976e9fffeb508e2b5c4531
MidgleyM <115593302+MidgleyM@users.noreply.github.com>
2024-03-03 18:15:03 -0600

    set up auto, needs testing.

--- robotbase/src/main/java/frc/robot/commands/intake/IntakeNoteFromFloor.java
+++ robotbase/src/main/java/frc/robot/commands/intake/IntakeNoteFromFloor.java
@@ -1,6 +1,8 @@
 package frc.robot.commands.intake;
 
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.INTAKE;
 import frc.robot.commands.rumble.RumbleDriverController;
@@ -25,6 +27,22 @@ public class IntakeNoteFromFloor extends SequentialCommandGroup {
         // Run backwards to keep out of shooter
         new IntakeRun(INTAKE.REVERSE_FEED_SPEED_PERCENT_OUTPUT)
             .withTimeout(INTAKE.FLOOR_INTAKE_REVERSE_TIMEOUT),
+        new IntakeStop(),
+        new SetNoteState(NoteState.NOTE_STOWED));
+  }
+
+  public IntakeNoteFromFloor(boolean rumbleController) {
+    super(
+        new IntakeRunUntilBeamState(INTAKE.PICKUP_SPEED_PERCENT_OUTPUT, true),
+        new SetNoteState(NoteState.NOTE_IN_INTAKE),
+        new ParallelDeadlineGroup(
+            new IntakeRunUntilBeamState(INTAKE.SLOW_PICKUP_SPEED_PERCENT_OUTPUT, false),
+            new ConditionalCommand(new RumbleDriverController(), new InstantCommand(), () -> rumbleController)),
+        new SetNoteState(NoteState.NOTE_PAST_BEAM_BREAK),
+        // new ParallelDeadlineGroup(
+        // new WaitCommand(INTAKE.FLOOR_INTAKE_REVERSE_TIMEOUT),
+        // new InstantCommand(() ->
+        // Robot.intake.set(INTAKE.REVERSE_FEED_SPEED_PERCENT_OUTPUT))),
         new IntakeStop(),
         new SetNoteState(NoteState.NOTE_STOWED));
   }
