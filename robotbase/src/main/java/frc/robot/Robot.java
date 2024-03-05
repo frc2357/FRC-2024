98743d06a8054ba52890e993bb4b31b24045947c
frc2357 <personman6000@gmail.com>
2024-03-04 19:06:31 -0600

    trying to fix git

--- robotbase/src/main/java/frc/robot/Robot.java
+++ robotbase/src/main/java/frc/robot/Robot.java
@@ -12,7 +12,6 @@ import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.drive.SetCoastOnDisable;
@@ -202,13 +201,7 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {
-    Robot.LEDs.setColor(
-        new Color(
-            (int) SmartDashboard.getNumber("DB/Slider 0", 0),
-            (int) SmartDashboard.getNumber("DB/Slider 1", 0),
-            (int) SmartDashboard.getNumber("DB/Slider 2", 0)));
-  }
+  public void teleopPeriodic() {}
 
   @Override
   public void testInit() {
