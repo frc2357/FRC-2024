3c92e291d85aa686733272705728b1a5acb6d16c
frc2357 <personman6000@gmail.com>
2024-03-04 18:59:10 -0600

    gradle + remembering to save

--- robotbase/src/main/java/frc/robot/Robot.java
+++ robotbase/src/main/java/frc/robot/Robot.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.drive.SetCoastOnDisable;
@@ -201,7 +202,13 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    Robot.LEDs.setColor(
+        new Color(
+            (int) SmartDashboard.getNumber("DB/Slider 0", 0),
+            (int) SmartDashboard.getNumber("DB/Slider 1", 0),
+            (int) SmartDashboard.getNumber("DB/Slider 2", 0)));
+  }
 
   @Override
   public void testInit() {
