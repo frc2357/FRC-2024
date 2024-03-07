bcd743b7613ae3c6749fd1b03f55e0d334591c6c
NolanCampbell2357 <nolanc1223@gmail.com>
2024-03-07 06:54:21 -0600

    Stop the shooter and intake

--- robotbase/src/main/java/frc/robot/commands/auto/paths/MiddleClose1Speaker.java
+++ robotbase/src/main/java/frc/robot/commands/auto/paths/MiddleClose1Speaker.java
@@ -5,6 +5,7 @@ import frc.robot.commands.auto.AutoPivotSetAngle;
 import frc.robot.commands.auto.AutoShooterSetRPMAndFinish;
 import frc.robot.commands.drive.DriveChoreoPath;
 import frc.robot.commands.intake.IntakeRun;
+import frc.robot.commands.shooter.ShooterStop;
 import frc.robot.commands.shooter.ShooterWaitForRPM;
 
 public class MiddleClose1Speaker extends SequentialCommandGroup {
@@ -13,7 +14,8 @@ public class MiddleClose1Speaker extends SequentialCommandGroup {
         new AutoPivotSetAngle(60),
         new AutoShooterSetRPMAndFinish(3000),
         new ShooterWaitForRPM().withTimeout(0.5),
-        new IntakeRun(0.75, false).withTimeout(1),
+        new IntakeRun(0.75, true).withTimeout(1),
+        new ShooterStop(),
         new DriveChoreoPath("MiddleClose1Speaker")
     );
   }
