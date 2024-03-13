47f5b79281f434d7f60e732a8f671b7461bd1d94
frc2357 <personman6000@gmail.com>
2024-03-13 18:44:11 -0500

    changes the LED color to suppliment the controller's rumble

--- robotbase/src/main/java/frc/robot/commands/intake/IntakeNoteFromFloor.java
+++ robotbase/src/main/java/frc/robot/commands/intake/IntakeNoteFromFloor.java
@@ -1,15 +1,21 @@
 package frc.robot.commands.intake;
 
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.INTAKE;
+import frc.robot.commands.LEDs.LEDsSetColor;
 import frc.robot.commands.rumble.RumbleDriverController;
 import frc.robot.commands.state.SetNoteState;
 import frc.robot.state.RobotState.NoteState;
+import frc.robot.subsystems.LEDs;
 
 public class IntakeNoteFromFloor extends SequentialCommandGroup {
   public IntakeNoteFromFloor() {
     super(
+      
+        new LEDsSetColor(LEDs.MELTDOWN_ORANGE),
+
         // Run until we pickup note
         new IntakeRunUntilBeamState(INTAKE.PICKUP_SPEED_PERCENT_OUTPUT, true),
         new SetNoteState(NoteState.NOTE_IN_INTAKE),
@@ -17,6 +23,7 @@ public class IntakeNoteFromFloor extends SequentialCommandGroup {
             () -> {
               new RumbleDriverController().schedule();
             }),
+        new LEDsSetColor(Color.kWhite),
 
         // Run slow until past beam break
         new IntakeRunUntilBeamState(INTAKE.SLOW_PICKUP_SPEED_PERCENT_OUTPUT, false),
@@ -27,5 +34,7 @@ public class IntakeNoteFromFloor extends SequentialCommandGroup {
             .withTimeout(INTAKE.FLOOR_INTAKE_REVERSE_TIMEOUT),
         new IntakeStop(),
         new SetNoteState(NoteState.NOTE_STOWED));
+
+        
   }
 }
