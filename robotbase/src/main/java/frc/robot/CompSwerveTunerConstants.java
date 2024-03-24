17549272d3c16ac1ca8599d47b7ce8d440f9d66b
Tyson453 <95186148+Tyson453@users.noreply.github.com>
2024-03-24 14:06:20 -0700

    Drive pids

--- robotbase/src/main/java/frc/robot/CompSwerveTunerConstants.java
+++ robotbase/src/main/java/frc/robot/CompSwerveTunerConstants.java
@@ -22,7 +22,7 @@ public class CompSwerveTunerConstants {
   // When using closed-loop control, the drive motor uses the control
   // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
   private static final Slot0Configs driveGains =
-      new Slot0Configs().withKP(.05).withKI(0.0).withKD(0.0).withKS(0.1).withKV(0.13).withKA(0);
+      new Slot0Configs().withKP(.05).withKI(0.0).withKD(0.0).withKS(0.1).withKV(1.3).withKA(0);
 
   /*
       new Slot0Configs().withKP(8.4).withKI(0)
