33fbd1410142fe7a5fd002addf746e9ddba83bab
Tyson453 <95186148+Tyson453@users.noreply.github.com>
2024-03-26 13:59:15 -0700

    Rename old drive to gamepiece

---
+++ robotbase/src/main/java/frc/robot/commands/drive/DriveToGamepeiceRobotRelative.java
@@ -0,0 +1,75 @@
+package frc.robot.commands.drive;
+
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.math.filter.Debouncer.DebounceType;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.INTAKE_PHOTON_CAMERA;
+import frc.robot.Constants.SWERVE;
+import frc.robot.Robot;
+import frc.robot.state.RobotState.DriveControlState;
+
+public class DriveToGamepeiceRobotRelative extends Command {
+  private Debouncer m_canSeePieceDebouncer;
+  private Pose2d m_initialPose;
+
+  public DriveToGamepeiceRobotRelative() {
+    addRequirements(Robot.swerve, Robot.intakeCam);
+  }
+
+  @Override
+  public void initialize() {
+    if (Robot.intakeCam.getPipeline() != INTAKE_PHOTON_CAMERA.NEURAL_NETWORK_PIPELINE) {
+      Robot.intakeCam.setPipeline(INTAKE_PHOTON_CAMERA.NEURAL_NETWORK_PIPELINE);
+    }
+    Robot.state.setDriveControlState(DriveControlState.ROBOT_RELATIVE);
+    SWERVE.TARGET_LOCK_ROTATION_PID_CONTROLLER.reset();
+    SWERVE.TARGET_LOCK_ROTATION_PID_CONTROLLER.setTolerance(
+        SWERVE.PIECE_TRACKING_ROTATION_TOLERANCE);
+
+    m_initialPose = Robot.swerve.getPose();
+    m_canSeePieceDebouncer =
+        new Debouncer(SWERVE.AUTO_TRANSLATE_DEBOUNCE_SECONDS, DebounceType.kFalling);
+  }
+
+  @Override
+  public void execute() {
+    /*
+    if (!m_canSeePieceDebouncer.calculate(Robot.intakeCam.validTargetExists())) {
+      System.out.println("No Gamepiece Detected");
+      Robot.swerve.stopMotors();
+      return;
+    }
+
+    double rotationError = Robot.intakeCam.getBestTargetYaw();
+    double rotationSpeed = SWERVE.TARGET_LOCK_ROTATION_PID_CONTROLLER.calculate(rotationError, 0);
+    double translationSpeed =
+        distanceTraveled() > SWERVE.PIECE_TRACKING_SLOW_DOWN_METERS
+            ? SWERVE.PIECE_TRACKING_X_METERS_PER_SECOND / 2.0
+            : SWERVE.PIECE_TRACKING_X_METERS_PER_SECOND;
+    Robot.swerve.driveRobotRelative(-translationSpeed, 0, rotationSpeed);
+    */
+  }
+
+  @Override
+  public boolean isFinished() {
+    return isDistanceTraveledTooFar(); // OR GAMEPEICE ACQUIRED
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    Robot.swerve.stopMotors();
+    SWERVE.TARGET_LOCK_ROTATION_PID_CONTROLLER.setTolerance(0);
+    Robot.state.setDriveControlState(DriveControlState.FIELD_RELATIVE);
+    System.out.println(interrupted);
+  }
+
+  private double distanceTraveled() {
+    return Math.abs(
+        Robot.swerve.getPose().getTranslation().getDistance(m_initialPose.getTranslation()));
+  }
+
+  private boolean isDistanceTraveledTooFar() {
+    return Math.abs(distanceTraveled()) > SWERVE.PIECE_TRACKING_MAX_DISTANCE_METERS;
+  }
+}
