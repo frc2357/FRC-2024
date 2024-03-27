0e30f18050847e85bf966347af6a7999a3689c4b
Max Midgley <115593302+MidgleyM@users.noreply.github.com>
2024-03-26 19:56:16 -0500

    filters notes better (#192)
    no longer switches between notes that are right next to each other.

--- robotbase/src/main/java/frc/robot/Constants.java
+++ robotbase/src/main/java/frc/robot/Constants.java
@@ -411,6 +411,8 @@ public final class Constants {
         "----------\nPHOTON VISION HAS LOST CONNECTION!\nVISION RESULTS WILL NOT BE UPDATED!\n----------";
     public static final String CONNECTION_REGAINED_NOFICATION_MESSAGE =
         "**********\nPhoton Vision has regained connection!\nVision results will now be updated.\n**********";
+
+    public static final double BEST_TARGET_PITCH_TOLERANCE_DEGREES = 5;
   }
 
   public static final class SHOOTER_PHOTON_CAMERA {
