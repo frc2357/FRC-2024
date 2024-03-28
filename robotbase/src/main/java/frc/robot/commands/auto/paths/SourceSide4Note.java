9b97057577eac496a51d4b045b82a1c3ab9f828f
Tyson453 <95186148+Tyson453@users.noreply.github.com>
2024-03-28 16:31:41 -0700

    Linting

--- robotbase/src/main/java/frc/robot/commands/auto/paths/SourceSide4Note.java
+++ robotbase/src/main/java/frc/robot/commands/auto/paths/SourceSide4Note.java
@@ -9,75 +9,71 @@ import frc.robot.commands.pivot.PivotHoldAngle;
 import frc.robot.commands.shooter.ShooterSetRPM;
 
 public class SourceSide4Note extends SequentialCommandGroup {
-   public SourceSide4Note() {
-      super(
-         // Preload on the move (future Tyson problem) + Drive to note 2
-         new ParallelDeadlineGroup(
+  public SourceSide4Note() {
+    super(
+        // Preload on the move (future Tyson problem) + Drive to note 2
+        new ParallelDeadlineGroup(
             new DriveChoreoPath("SourceSide4Note1.1"),
             new ParallelDeadlineGroup(
-               new SequentialCommandGroup(
-                  new WaitCommand(1),
-                  new IntakeFeedToShooter().withTimeout(0.25)
-               ),
-               new PivotHoldAngle(45),
-               new ShooterSetRPM(3500)
-            )
-         )
+                new SequentialCommandGroup(
+                    new WaitCommand(1), new IntakeFeedToShooter().withTimeout(0.25)),
+                new PivotHoldAngle(45),
+                new ShooterSetRPM(3500)))
 
-         // Pickup second note
-         // new ParallelDeadlineGroup(
-         //    new TranslateToGamepiece(),
-         //    new IntakeNoteFromFloor()
-         // ),
+        // Pickup second note
+        // new ParallelDeadlineGroup(
+        //    new TranslateToGamepiece(),
+        //    new IntakeNoteFromFloor()
+        // ),
 
-         // // Drive back with note 2 and shoot
-         // new ParallelDeadlineGroup(
-         //    new SequentialCommandGroup(
-         //       new DriveChoreoPath("SourceSide4Note2.1"),
-         //       new TargetLockOnSpeaker().withTimeout(SWERVE.AUTO_TARGET_LOCK_TIMEOUT_SECONDS),
-         //       new IntakeFeedToShooter().withTimeout(0.2)
-         //    ),
-         //    new VisionTargeting()
-         // ),
+        // // Drive back with note 2 and shoot
+        // new ParallelDeadlineGroup(
+        //    new SequentialCommandGroup(
+        //       new DriveChoreoPath("SourceSide4Note2.1"),
+        //       new TargetLockOnSpeaker().withTimeout(SWERVE.AUTO_TARGET_LOCK_TIMEOUT_SECONDS),
+        //       new IntakeFeedToShooter().withTimeout(0.2)
+        //    ),
+        //    new VisionTargeting()
+        // ),
 
-         // // Drive to and pickup third note
-         // new DriveChoreoPath("SourceSide4Note2.2"),
-         // new ParallelDeadlineGroup(
-         //    new TranslateToGamepiece(),
-         //    new IntakeNoteFromFloor()
-         // ),
+        // // Drive to and pickup third note
+        // new DriveChoreoPath("SourceSide4Note2.2"),
+        // new ParallelDeadlineGroup(
+        //    new TranslateToGamepiece(),
+        //    new IntakeNoteFromFloor()
+        // ),
 
-         // // Drive back with note 3 and shoot
-         // new ParallelDeadlineGroup(
-         //    new SequentialCommandGroup(
-         //       new DriveChoreoPath("SourceSide4Note3.1"),
-         //       new TargetLockOnSpeaker().withTimeout(SWERVE.AUTO_TARGET_LOCK_TIMEOUT_SECONDS),
-         //       new IntakeFeedToShooter().withTimeout(0.2)
-         //    ),
-         //    new VisionTargeting()
-         // ),
+        // // Drive back with note 3 and shoot
+        // new ParallelDeadlineGroup(
+        //    new SequentialCommandGroup(
+        //       new DriveChoreoPath("SourceSide4Note3.1"),
+        //       new TargetLockOnSpeaker().withTimeout(SWERVE.AUTO_TARGET_LOCK_TIMEOUT_SECONDS),
+        //       new IntakeFeedToShooter().withTimeout(0.2)
+        //    ),
+        //    new VisionTargeting()
+        // ),
 
-         // // Drive to and pickup fourth note
-         // new DriveChoreoPath("SouceSide4Note3.2"),
-         // new ParallelDeadlineGroup(
-         //    new TranslateToGamepiece(),
-         //    new IntakeNoteFromFloor()
-         // ),
+        // // Drive to and pickup fourth note
+        // new DriveChoreoPath("SouceSide4Note3.2"),
+        // new ParallelDeadlineGroup(
+        //    new TranslateToGamepiece(),
+        //    new IntakeNoteFromFloor()
+        // ),
 
-         // // Drive back with note 4 and shoot
-         // new ParallelDeadlineGroup(
-         //    new SequentialCommandGroup(
-         //       new DriveChoreoPath("SourceSide4Note4.1"),
-         //       new TargetLockOnSpeaker().withTimeout(SWERVE.AUTO_TARGET_LOCK_TIMEOUT_SECONDS),
-         //       new IntakeFeedToShooter().withTimeout(0.2)
-         //    ),
-         //    new VisionTargeting()
-         // )
-      );
-   }
+        // // Drive back with note 4 and shoot
+        // new ParallelDeadlineGroup(
+        //    new SequentialCommandGroup(
+        //       new DriveChoreoPath("SourceSide4Note4.1"),
+        //       new TargetLockOnSpeaker().withTimeout(SWERVE.AUTO_TARGET_LOCK_TIMEOUT_SECONDS),
+        //       new IntakeFeedToShooter().withTimeout(0.2)
+        //    ),
+        //    new VisionTargeting()
+        // )
+        );
+  }
 
-   @Override
-   public String toString() {
-      return "SourceSide4Note";
-   }
+  @Override
+  public String toString() {
+    return "SourceSide4Note";
+  }
 }
