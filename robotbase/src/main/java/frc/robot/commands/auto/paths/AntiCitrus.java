f8b2c010be4aae03b18ac685c37754c4a20f8120
NolanCampbell2357 <nolanc1223@gmail.com>
2024-04-25 18:20:20 -0500

    formatting

--- robotbase/src/main/java/frc/robot/commands/auto/paths/AntiCitrus.java
+++ robotbase/src/main/java/frc/robot/commands/auto/paths/AntiCitrus.java
@@ -1,14 +1,9 @@
 package frc.robot.commands.auto.paths;
 
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.Constants.SWERVE;
-import frc.robot.Robot;
 import frc.robot.commands.auto.AutoPickup;
 import frc.robot.commands.drive.DriveChoreoPath;
 import frc.robot.commands.drive.TargetLockOnSpeaker;
@@ -23,10 +18,10 @@ import frc.robot.commands.shooter.ShooterWaitForRPM;
 public class AntiCitrus extends SequentialCommandGroup {
 
   public AntiCitrus() {
-   super(
+    super(
         // Preload on the move (future Tyson problem) + Drive to note 2
         new ParallelDeadlineGroup(
-                new DriveChoreoPath("AntiCitrus1Blue.1", true),
+            new DriveChoreoPath("AntiCitrus1Blue.1", true),
             new SequentialCommandGroup(
                 new WaitCommand(0.75), new IntakeFeedToShooter().withTimeout(0.25)),
             new PivotHoldAngle(35.5),
@@ -39,19 +34,16 @@ public class AntiCitrus extends SequentialCommandGroup {
         // Drive back with note 2 and shoot
         new ParallelDeadlineGroup(
             new SequentialCommandGroup(
-                new DriveChoreoPath("AntiCitrus2.1", false)
-                    .deadlineWith(new AutoPickup()),
+                new DriveChoreoPath("AntiCitrus2.1", false).deadlineWith(new AutoPickup()),
                 new ParallelCommandGroup(new TargetLockOnSpeaker(true), new ShooterWaitForRPM())
                     .withTimeout(1.5),
                 new IntakeFeedToShooter().withTimeout(1)),
             new VisionTargeting(4800)),
         new ShooterStop(),
-
         new ShooterStop());
   }
 
   public String toString() {
     return "ANTI-CITRUS";
   }
-
 }
