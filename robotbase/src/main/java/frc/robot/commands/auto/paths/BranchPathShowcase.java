23258aa6ac35cba9a544665e8ec08a66ed2304e7
MidgleyM <115593302+MidgleyM@users.noreply.github.com>
2024-08-06 20:04:33 -0500

    its already becoming spaget

--- robotbase/src/main/java/frc/robot/commands/auto/paths/BranchPathShowcase.java
+++ robotbase/src/main/java/frc/robot/commands/auto/paths/BranchPathShowcase.java
@@ -30,17 +30,40 @@ public class BranchPathShowcase extends SequentialCommandGroup {
             new VisionTargeting(4800)),
         // Here comes the decision tree. I am dearly sorry to those who hate indents, for this is sin.
         new PathNode( 
-        new SequentialCommandGroup(
-            new DriveChoreoPath("BPNote2:B1T"),
-            new ParallelDeadlineGroup(new TranslateToGamepiece(3), new AutoPickup())
-        )
+            new SequentialCommandGroup(
+                new DriveChoreoPath("BPNote2:N1T"),
+                new ParallelDeadlineGroup(new TranslateToGamepiece(3), new AutoPickup()),
+                        // Drive back with note 2 and shoot
+                new ParallelDeadlineGroup(
+                    new SequentialCommandGroup(
+                    new DriveChoreoPath("AmpSide4Note2WallFirst.1", false)
+                        .deadlineWith(new AutoPickup()),
+                    new ParallelCommandGroup(new TargetLockOnSpeaker(true), new ShooterWaitForRPM())
+                        .withTimeout(SWERVE.AUTO_TARGET_LOCK_TIMEOUT_SECONDS),
+                    new IntakeFeedToShooter().withTimeout(0.2)), new VisionTargeting(4800)),
+            new PathNode(null, null, null, "[BP1] Node 2"))
         ,
-        new SequentialCommandGroup(
-            new DriveChoreoPath("BPNote2:B1F"),
-            new ParallelDeadlineGroup(new TranslateToGamepiece(3), new AutoPickup())
+            new SequentialCommandGroup(
+                new DriveChoreoPath("BPNote2:N1F"),
+                new ParallelDeadlineGroup(new TranslateToGamepiece(3), new AutoPickup()),
+                // Drive back with note 2 and shoot
+                new ParallelDeadlineGroup(
+                    new SequentialCommandGroup(
+                        new DriveChoreoPath("AmpSide4Note2.1", false, true).deadlineWith(new AutoPickup()),
+                        new ParallelCommandGroup(new TargetLockOnSpeaker(true), new ShooterWaitForRPM())
+                            .withTimeout(SWERVE.AUTO_TARGET_LOCK_TIMEOUT_SECONDS),
+                        new IntakeFeedToShooter().withTimeout(0.2)),
+                    new VisionTargeting(4800)),
+                new PathNode(
+                    
+                , 
+                
+                , 
+                
+                , "[BP1] Node 3")
         )
         ,     
-        () -> (Math.abs(Robot.intakeCam.getNoteTargetPitch()) <= m_secondNotePitchTolerance), "[BP1] Note2:B1 Decision Node")
+        () -> (Math.abs(Robot.intakeCam.getNoteTargetPitch()) <= m_secondNotePitchTolerance), "[BP1] Node 1")
         );
   }
 
